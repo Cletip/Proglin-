0c88b45e5a91bfc3e6c30cfd64638c239b546819
Mathieu Laurencot <mathieu.laurencot@etu.uca.fr>
2021-09-26 18:34:13 +0200

    Résolution de Gauss + multiplication

--- fonction.h
+++ fonction.h
@@ -26,9 +26,12 @@ void remplisAleaBcpZero(matrice *mat);
 void remplisAlea(matrice *mat);
 void remplisAleaInt(matrice *mat);
 matrice *additionMatrice(matrice mat1, matrice mat2);
+matrice *multiplicationMatrice(matrice mat1, matrice mat2);
 matrice *Jacobi(matrice *A, matrice *B);
 void InversematriceD(int taille, matrice *D);
 
 matrice *Gauss(matrice mat);
+matrice *ResolutionParGauss(matrice A, matrice B);
+
 
 #endif
